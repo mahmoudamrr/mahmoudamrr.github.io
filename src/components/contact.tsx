185a5c5ca2a12ff476036aca96de662c565b1549
mahmoudamrr <mahmoud.mansour.amr.mm@gmail.com>
2021-09-25 09:27:33 +0200

    pic

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -8,28 +8,6 @@ import { UpDown, UpDownWide, waveAnimation } from "../styles/animations";
 import Footer from "./footer";
 // @ts-ignore
 import ContactMDX from "../sections/contact.mdx";
-
-const size = {
-  mobileS: "320px",
-  mobileM: "375px",
-  mobileL: "425px",
-  tablet: "768px",
-  laptop: "1024px",
-  laptopL: "1440px",
-  desktop: "2560px",
-};
-
-export const device = {
-  mobileS: `(min-width: ${size.mobileS})`,
-  mobileM: `(min-width: ${size.mobileM})`,
-  mobileL: `(min-width: ${size.mobileL})`,
-  tablet: `(min-width: ${size.tablet})`,
-  laptop: `(min-width: ${size.laptop})`,
-  laptopL: `(min-width: ${size.laptopL})`,
-  desktop: `(min-width: ${size.desktop})`,
-  desktopL: `(min-width: ${size.desktop})`,
-};
-
 const Contact = ({
   offset,
   factor = 1,
